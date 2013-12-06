f04718041b49ad230e326a071e0a1a825e3db2c0
Dominic Garms <djgarms@gmail.com>
2013-12-06 16:08:52 +0800

    Initial commit

---
+++ Classes/DMF/BootstrapContent/Controller/StandardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace DMF\BootstrapContent\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "DMF.BootstrapContent".  *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
+	}
+
+}
+
+?>
